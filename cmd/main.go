d3f8e987f09cfcda58dc6c1c6a3b1a9d9601f744
Dinesh Thakur <t.diku63@gmail.com>
2023-04-07 18:49:04 +0530

    updated main with new changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"HexArithmatics/internal/adapters/app/api"
-	"HexArithmatics/internal/adapters/core/arithmetic"
 	gRPC "HexArithmatics/internal/adapters/framework/left/grpc"
 	"HexArithmatics/internal/adapters/framework/right/db"
-	"HexArithmatics/internal/ports"
+	"HexArithmatics/internal/application/api"
+	"HexArithmatics/internal/application/core/arithmetic"
 	"os"
 
 	"github.com/sirupsen/logrus"
@@ -14,26 +13,21 @@ import (
 func main() {
 	var err error
 
-	//Ports
-	var dbaseAdapter ports.DBPort
-	var core ports.ArthimeticsPort
-	var appAdapter ports.APIPort
-	var gRPCAdpter ports.GRPCPort
-
 	dBaseDrive := os.Getenv("DB_DRIVER")
 	dSourceName := os.Getenv("DS_NAME")
 
-	dbaseAdapter, err = db.NewAdapter(dBaseDrive, dSourceName)
+	dbaseAdapter, err := db.NewAdapter(dBaseDrive, dSourceName)
 	if err != nil {
 		logrus.Fatalf("Failed to intitate database connections: %v", err)
 	}
 	defer dbaseAdapter.CloseDbConnection()
 
-	core = arithmetic.NewAdapter()
+	//core
+	core := arithmetic.New()
 
-	appAdapter = api.NewAdapter(dbaseAdapter, core)
+	applicationAPI := api.NewApplication(dbaseAdapter, core)
 
-	gRPCAdpter = gRPC.NewAdapter(appAdapter)
+	gRPCAdpter := gRPC.NewAdapter(applicationAPI)
 
 	gRPCAdpter.Run()
 
